0b8fb865bc73938c98639346021b45349c345396
gongbaodd@Inspiron13 <gongbaodd@qq.com>
2020-05-20 16:39:12 +0800

    ✨ Feat: remove some files

--- src/routes/1x03-diagnal-60-stripes.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Backgrounds from "../components/Backgrounds.svelte";
-
-  let bgStyle = `
-  background: repeating-linear-gradient(60deg, hsla(0, 0%, 100%, .5), hsla(0, 0%, 100%, .5) 15px, transparent 0, transparent 30px);
-  `;
-</script>
-
-<Backgrounds {bgStyle} />
--- src/routes/3x01-diamond.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Border from "../components/Borders.svelte";
-
-  let BORDER_STYLE = `
-clip-path: polygon(50% 0, 100% 50%, 50% 100%, 0 50%);
-`;
-</script>
-
-<Border style={BORDER_STYLE} />
